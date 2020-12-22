a146b89b985a8e9bbe09a39b091309a3105fa9f0
Alexander Swed <lexswed@gmail.com>
2020-12-22 21:56:35 +0100

    update versions

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,10 +1,10 @@
 import { NextApiHandler } from 'next'
 
 const apps = {
-  'app-shell': '642918d901432ea54fda589fe36f0ede1029984a',
+  'app-shell': '61f403dbfbc060d0ffb9820f8bfa64ca9117ca16',
   'chief-app': '33d61fc4bcceee09bae14ca962dfa39ba611d1c7',
-  'hall-of-forms': '44b1cf0a1cc1b057852bb7b38d2fe478ea703226',
-  'integrations-panel': 'dd54b52242396fbb9030c0708f71b7558670fbf9',
+  'hall-of-forms': '557c63c8afeabdbd74d78df56d90fbb322f671a5',
+  'integrations-panel': '02cbc0cdc638c1069930e07263ada08b008d5195',
   'paprikations': '1987ffd8d0793cbb4fbc266e3a8f80202e99f76d',
 }
 
