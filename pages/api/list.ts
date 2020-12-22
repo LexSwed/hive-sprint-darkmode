578b31055a357f3829ec9f0b2cc2e7fc0325b754
Alexander Swed <lexswed@gmail.com>
2020-12-22 20:40:14 +0100

    fix chief-app

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -2,7 +2,7 @@ import { NextApiHandler } from 'next'
 
 const apps = {
   'app-shell': '642918d901432ea54fda589fe36f0ede1029984a',
-  'chief': '33d61fc4bcceee09bae14ca962dfa39ba611d1c7',
+  'chief-app': '33d61fc4bcceee09bae14ca962dfa39ba611d1c7',
   'hall-of-forms': '44b1cf0a1cc1b057852bb7b38d2fe478ea703226',
   'integrations-panel': 'dd54b52242396fbb9030c0708f71b7558670fbf9',
   'paprikations': '1987ffd8d0793cbb4fbc266e3a8f80202e99f76d',
