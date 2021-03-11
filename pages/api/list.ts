b14cc6dcefb4f45f2de32ef41263baff0ea71133
Alexander Swed <lexswed@gmail.com>
2021-03-11 09:49:29 +0100

    update apps

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,12 +1,8 @@
 import { NextApiHandler } from 'next'
 
 const apps = {
-  'app-shell': '8033ee457ace4ebc10f05827c45e27ee315f6455',
-  'chief-app': '33d61fc4bcceee09bae14ca962dfa39ba611d1c7',
-  'hall-of-forms': '15eabe49cac4f541c08715046807da176d0f0f3d',
-  'integrations-panel': '28b912d2d41c4332504244460e9b29199d0efcd3',
-  'paprikations': '25f16a959745c847f68995513e28876d3fb3fbf6',
-  'shareinguito': '7cbab525bde72876ce356a776f4203f6a0b6e261',
+  'app-shell': '509ad4e3a19cb41f6dc14a9b83c9e4c042a40fea',
+  'chief-app': 'ea9abe2311f4a160048b06d57e38ff20526f40d1',
 }
 
 const handler: NextApiHandler = (req, res) => {
